45a3a2859f3d780ea079c56ab09c733f3196ace9
NezertorcheaT <tigrisha06ps@gmail.com>
2024-02-04 01:12:18 +0300

    summaries

--- Assets/Scripts/Entity/States/IStateTree.cs
+++ Assets/Scripts/Entity/States/IStateTree.cs
@@ -2,23 +2,98 @@
 
 namespace Entity.States
 {
-#if true
-    
     public interface IStateTree
     {
+        /// <summary>
+        /// ну вы бля поняли, хэш
+        /// </summary>
+        /// <param name="state"></param>
+        /// <returns></returns>
         int Hash(IState state);
+
+        /// <summary>
+        /// добавить состояние в общий пул
+        /// </summary>
+        /// <param name="state">состояние</param>
         void AddState(IState state);
+
+        /// <summary>
+        /// попытаться соеденить два состояния
+        /// </summary>
+        /// <param name="idA">айди того куда подсоединять</param>
+        /// <param name="idB">айди того что подсоединять</param>
+        /// <returns>получилось или нет</returns>
         bool TryConnect(int idA, int idB);
+
+        /// <summary>
+        /// попытаться открепить два состояния
+        /// </summary>
+        /// <param name="idA">айди того откуда открепить</param>
+        /// <param name="idB">айди того что открепить</param>
+        /// <returns>получилось или нет</returns>
         bool TryDisconnect(int idA, int idB);
+
+        /// <summary>
+        /// попытаться открепить состояние от вообще всего
+        /// </summary>
+        /// <param name="id">айди того что открепить</param>
+        /// <returns>получилось или нет</returns>
         bool TryDisconnect(int id);
+
+        /// <summary>
+        /// находит все айди состояний, в Next которых есть нужное
+        /// </summary>
+        /// <param name="id">айди состояния</param>
+        /// <returns>массив айди</returns>
         int[] FindConnections(int id);
+
+        /// <summary>
+        /// попытаться безопасно удалить состояние из пула
+        /// </summary>
+        /// <param name="id">айди состояния</param>
+        /// <returns>получилось или нет</returns>
         bool TryRemoveState(int id);
+
+        /// <summary>
+        /// проверить, является ли число корректны айди
+        /// </summary>
+        /// <param name="id">айди</param>
+        /// <returns></returns>
         bool IsIdValid(int id);
+
+        /// <summary>
+        /// попытаться получить состояние по айди
+        /// </summary>
+        /// <param name="id">айди</param>
+        /// <param name="state">ссылка на выходное значение состояния</param>
+        /// <returns>получилось или нет</returns>
         bool TryGetState(int id, ref IState state);
+
+        /// <summary>
+        /// получить состояние по айди на прямую
+        /// </summary>
+        /// <param name="id">айди</param>
+        /// <returns>состояние</returns>
         IState GetState(int id);
+
+        /// <summary>
+        /// получить состояния следующие после этого состояния
+        /// </summary>
+        /// <param name="id">айди</param>
+        /// <returns>состояния</returns>
         IState[] GetNextsTo(int id);
+
+        /// <summary>
+        /// получить начальное состояние
+        /// не путать с GetState(0)
+        /// это разные вещи
+        /// </summary>
+        /// <returns>состояние</returns>
         IState First();
+
+        /// <summary>
+        /// все состояния в соответствии с их айди
+        /// </summary>
         Dictionary<int, IState> States { get; }
     }
-#endif
 }
--- Assets/Scripts/Entity/States/State.cs
+++ Assets/Scripts/Entity/States/State.cs
@@ -9,17 +9,41 @@ namespace Entity.States
     [Serializable]
     public abstract class State : ScriptableObject, IState
     {
+        /// <summary>
+        /// бесполезно, пока я эдитор не сделаю
+        /// </summary>
         string IState.Name => Name;
+        /// <summary>
+        /// бесполезно, пока я эдитор не сделаю
+        /// </summary>
         protected abstract string Name { get; }
 
+        /// <summary>
+        /// не менять пж
+        /// </summary>
         int IState.Id
         {
             get => Id;
             set => Id = value;
         }
 
+        /// <summary>
+        /// не менять пж
+        /// </summary>
         protected abstract int Id { get; set; }
+        /// <summary>
+        /// асинхронный метод собственно действия
+        /// </summary>
+        /// <param name="entity">над кем будет действие произведено</param>
+        /// <param name="previous">предыдущее состояние, хз может нужно кому</param>
+        /// <returns>должен вернуть номер следующего в массиве следующих состояний</returns>
         protected abstract Task<int> Activate(Entity entity, IState previous);
+        /// <summary>
+        /// асинхронный метод собственно действия
+        /// </summary>
+        /// <param name="entity">над кем будет действие произведено</param>
+        /// <param name="previous">предыдущее состояние, хз может нужно кому</param>
+        /// <returns>должен вернуть номер следующего в массиве следующих состояний</returns>
         Task<int> IState.Activate(Entity entity, IState previous) => Activate(entity, previous);
     }
 }
