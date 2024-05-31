dc67167506d7e0386b624a616a9c81f31fd6f5f1
Xesh <132228183+XeshSufferer@users.noreply.github.com>
2024-05-31 20:28:23 +0200

    Jump fix
    Ураа райкаст не считает коллайдер игрока за землю сюдоо
    (А и еще теперь AddForce а не velocity)

--- Assets/Scripts/Entity/PlayerJumpAbility.cs
+++ Assets/Scripts/Entity/PlayerJumpAbility.cs
@@ -2,11 +2,25 @@ using UnityEngine;
 
 namespace Entity.Abilities
 {
-    public class PlayerJumpAbility : Ability
-    {
+public class PlayerJumpAbility : Ability
+{
+
+    //private Rigidbody2D _playerRb;
+
+
+    //private int jumpCountActive;
+
+    //[Space(10.0f), SerializeField, Min(1)] private int jumpCount = 1;
+    //[SerializeField] private float jumpHeight = 3;
+
+
+
         [Space(10.0f), SerializeField, Min(1)] private int jumpCount = 1;
         [SerializeField] private float jumpHeight = 3;
-        [SerializeField] private float groundDistance = 3;
+        [Space(10f)]
+        [SerializeField, Min(-2f)] private float rayOffsetY;
+        [Space(10f)]
+        [SerializeField, Min(0.1f)] private float rayGroundRange = 3;
 
         private Rigidbody2D _playerRb;
         private int jumpCountActive;
@@ -23,11 +37,31 @@ namespace Entity.Abilities
         public void Jump()
         {
             if (jumpCountActive == 0) return;
-            _playerRb.velocity = new Vector2(_playerRb.velocity.x, jumpHeight);
+            _playerRb.AddForce(new Vector2(_playerRb.velocity.x, jumpHeight), ForceMode2D.Impulse);
             jumpCountActive -= 1;
         }
 
         private bool CheckGround() =>
-            Physics2D.Raycast(_playerRb.position, -_playerRb.transform.up, groundDistance).collider != null;
+            Physics2D.Raycast(new Vector2(_playerRb.position.x, _playerRb.position.y + rayOffsetY), -_playerRb.transform.up, rayGroundRange).collider != null;
+        
+        private void Update()
+        {
+            Debug.DrawRay(new Vector2(_playerRb.position.x, _playerRb.position.y + rayOffsetY), -_playerRb.transform.up * rayGroundRange, Color.yellow);
+        }
+    
+    /*
+    private bool CheckGround()
+    {
+        RaycastHit2D CheckgroundHit = Physics2D.Raycast(new Vector2(_playerRb.position.x, _playerRb.position.y + rayOffsetY), -_playerRb.transform.up, rayGroundRange);
+        if(CheckgroundHit.collider !=  null)
+        {
+            return true;
+        }else{
+            return false;
+        }
+    
     }
+    */
+}
+
 }
