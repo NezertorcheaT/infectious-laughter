0039aad61f760164bd39959185a27a795cc288bd
Xesh <132228183+XeshSufferer@users.noreply.github.com>
2024-06-12 19:34:20 +0200

    kapkan stoit i dashe rabotaet no...
    Ебитесь сами с его размещением и удалением из инвентаря. Я глянул че там в инвентаре и у меня чуть ебало не лопнуло к хуям

--- Assets/Scripts/Entity/Abilities/EntityMovementHorizontalMove.cs
+++ Assets/Scripts/Entity/Abilities/EntityMovementHorizontalMove.cs
@@ -7,6 +7,8 @@ namespace Entity.Abilities
     public class EntityMovementHorizontalMove : Ability
     {
         [SerializeField] private float speed;
+        [SerializeField] public bool CanWalk = true;
+        private bool entityHasStop = false;
         private Rigidbody2D _rb;
 
         private void Start()
@@ -17,8 +19,15 @@ namespace Entity.Abilities
         public void Move(float velocity)
         {
             if (!Available()) return;
-
-            _rb.velocity = new Vector2(velocity * speed, _rb.velocity.y);
+            if (CanWalk)
+            {
+                _rb.velocity = new Vector2(velocity * speed, _rb.velocity.y);
+                entityHasStop = false;
+            }else if(!entityHasStop)
+            {
+                _rb.velocity += -_rb.velocity;
+                entityHasStop = true;
+            }
         }
     }
 }
