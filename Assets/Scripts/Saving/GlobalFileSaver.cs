32f73bae53c572981562f628ebafcd818e1de744
NezertorcheaT <tigrisha06ps@gmail.com>
2024-06-20 12:30:19 +0300

    System.Text.Json.8.0.3 added
    pool of shit

--- Assets/Scripts/Saving/GlobalFileSaver.cs
+++ Assets/Scripts/Saving/GlobalFileSaver.cs
@@ -1,5 +1,4 @@
 ﻿using System.IO;
-using Newtonsoft.Json;
 using UnityEngine;
 
 namespace Saving
@@ -7,5 +6,31 @@ namespace Saving
     public static class GlobalFileSaver
     {
         public static string Path => Application.persistentDataPath;
+        /*public static Configs ReadFromDrive()
+        {
+            if (!File.Exists(Path))
+                new Configs().SaveToDrive();
+        
+            var fs = new FileStream(Path, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            var stream = new StreamReader(fs);
+            var str = JsonNode
+                .Parse(stream.ReadToEnd())
+                .Deserialize<Configs>()!;
+            stream.Close();
+            fs.Close();
+            return str;
+        }
+
+        public void SaveToDrive()
+        {
+            File.Delete(Path);
+            var fs = new FileStream(Path, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+            var file = JsonSerializer.Serialize(this);
+            var stream = new StreamWriter(fs);
+            stream.Flush();
+            stream.WriteLine(file);
+            stream.Close();
+            fs.Close();
+        }*/
     }
 }
