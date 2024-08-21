c9028f2541fa8191ac30af90a3b05106c5e1ff26
XeshSufferer <132228183+XeshSufferer@users.noreply.github.com>
2024-08-21 13:15:41 +0200

    FruitOfAspiration
    нуу ээ окэй если через обычный инвентарь..

---
+++ Assets/Scripts/Inventory/Items/FruitOfAspiration.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace Inventory.Items
+{
+    [CreateAssetMenu(fileName = "New Fruit Of Aspiration Item", menuName = "Inventory/Items/Fruit Of Aspiration", order = 0)]
+    public class FruitOfAspiration : ScriptableObject, IItem, IUsableItem
+    {
+        public string Name => "Fruit Of Aspiration";
+        public string Id => "il.fruit_of_aspiration";
+        public ScriptableObject SelfRef => this;
+        public Sprite Sprite => sprite;
+
+        [SerializeField] private Sprite sprite;
+        [field: SerializeField, Min(1)] public int ItemCost { get; private set; } = 1;
+        [field: SerializeField, Min(1)] public int MaxStackSize { get; private set; } = 1;
+
+        private Entity.Abilities.HorizontalMovement _playerMovement;
+        private float _chachedSpeed;
+        private bool _used = false;
+
+        public void Use(Entity.Entity entity, IInventory inventory, ISlot slot)
+        {
+            _playerMovement ??= entity.gameObject.GetComponent<Entity.Abilities.HorizontalMovement>();
+            _chachedSpeed = _chachedSpeed == 0 ? _playerMovement.Speed : _chachedSpeed;
+            _playerMovement.Speed = _used ? _chachedSpeed : _chachedSpeed * 1.5f;
+            _used = !_used;
+        }
+    }
+}
