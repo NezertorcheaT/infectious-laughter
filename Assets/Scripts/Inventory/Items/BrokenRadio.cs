b9eb5ad015fdf92e9a1bedc8f2bda685c08316d6
SfarxX <alupachiov@mail.ru>
2024-09-05 22:29:49 +0300

    Implemented Use method for radio

--- Assets/Scripts/Inventory/Items/BrokenRadio.cs
+++ Assets/Scripts/Inventory/Items/BrokenRadio.cs
@@ -1,3 +1,4 @@
+using Inventory.Input;
 using UnityEngine;
 
 namespace Inventory.Items
@@ -12,12 +13,18 @@ namespace Inventory.Items
         public ItemAdderVerifier Verifier { get; set; }
 
         [SerializeField] private Sprite sprite;
+        [SerializeField] private GameObject radioWorld;
         [field: SerializeField, Min(1)] public int ItemCost { get; private set; } = 1;
         [field: SerializeField, Min(1)] public int MaxStackSize { get; private set; } = 1;
 
         public void Use(Entity.Entity entity, IInventory inventory, ISlot slot)
         {
-            throw new System.NotImplementedException();
+            var position = entity.gameObject.transform.position;
+            var radio = Verifier.Container.InstantiatePrefab(radioWorld, position, Quaternion.identity, null);
+            radio.transform.SetParent(null);
+            var adder = radio.GetComponent<IItemAdder>();
+            adder.Input ??= entity.FindAvailableAbilityByInterface<IInventoryInput>();
+            slot.Count--;
         }
     }
 }
