5c641ebee4dce32b86ea47243bf5d51331a8c973
XeshSufferer <132228183+XeshSufferer@users.noreply.github.com>
2024-09-06 12:27:44 +0200

    Drugaya Sistema Proverki Inventorya v Blessing Place
    hui

--- Assets/Scripts/PropsImpact/BlessingPlace.cs
+++ Assets/Scripts/PropsImpact/BlessingPlace.cs
@@ -11,30 +11,16 @@ namespace PropsImpact
         [SerializeField] private ScriptableObject[] canSpawnItems;
         [Inject] private PlayerInstallation _player;
         private Inventory.Input.PlayerInventoryInput _input;
-        private List<Inventory.ISlot> _slots;
 
         private void Start()
         {
             _input = _player.Entity.FindAbilityByType<Inventory.Input.PlayerInventoryInput>();
-            _slots = _player.Inventory.Slots;
-        }
-
-        private bool CheckInventoryOnSpace()
-        {
-            for(int i = 0; i != _slots.Count; i++)
-            {
-                if(_slots[i].IsEmpty)
-                {
-                    return true;
-                }
-            }
-            return false;
         }
 
         public void Use()
         {
             int i = Random.Range(0, canSpawnItems.Length);
-            if(!CheckInventoryOnSpace()) return;
+            if(!_input.HasSpace((Inventory.IItem)canSpawnItems[i])) return;
             _input.AddItem(canSpawnItems[i]);
             Destroy(gameObject);
         }
