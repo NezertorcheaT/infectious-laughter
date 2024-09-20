a0aaee86bda4c33146e3e632a1ffc870dd3d304b
SfarxX <alupachiov@mail.ru>
2024-09-20 18:30:07 +0300

    feature implemented

--- Assets/Scripts/Entity/Abilities/WalkAndJumpParticle.cs
+++ Assets/Scripts/Entity/Abilities/WalkAndJumpParticle.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 namespace Entity.Abilities
@@ -7,28 +8,29 @@ namespace Entity.Abilities
     public class WalkAndJumpParticle : Ability
     {
         [SerializeField] private ParticleSystem walkParticle;
+        [Range(0, 1.0f)]
+        [SerializeField] float dustFormationPeriod = 0.2f;
         private Rigidbody2D _playerRb;
         private CollideCheck _collideCheck;
+        private float _counter;
 
-        private void Start()
+        private void Awake()
         {
             _collideCheck = Entity.FindExactAbilityByType<CollideCheck>();
             _playerRb = GetComponent<Rigidbody2D>();
         }
+
         private void Update()
         {
-            if (_collideCheck.IsTouchingGround) PerformWalk();
+            _counter += Time.deltaTime;
+            if (_collideCheck.IsTouchingGround && (Mathf.Abs(_playerRb.velocity.x) > 0.0f))
+            {
+                if(_counter > dustFormationPeriod)
+                {
+                    _counter = 0;
+                    walkParticle.Play();
+                }
+            }
         }
-
-        public void PerformWalk()
-        {
-            walkParticle.Play();
-        }
-
-        public void UndoPerform()
-        {
-            walkParticle.Stop();
-        }
-
     }
 }
