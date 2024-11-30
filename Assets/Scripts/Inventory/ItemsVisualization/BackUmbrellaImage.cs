adde0b3e82458e026d939df19b763266d0ee30a2
XeshSufferer <132228183+XeshSufferer@users.noreply.github.com>
2024-11-30 17:36:45 +0200

    swap umbrella sprite

--- Assets/Scripts/Inventory/ItemsVisualization/BackUmbrellaImage.cs
+++ Assets/Scripts/Inventory/ItemsVisualization/BackUmbrellaImage.cs
@@ -9,22 +9,17 @@ public class BackUmbrellaImage : MonoBehaviour
 
     [SerializeField] private SpriteRenderer backSpriteRenderer;
     [SerializeField] private Entity.Abilities.LightResponsive playerLightResponse;
-    [SerializeField] private Entity.Abilities.HorizontalMovement playerHorizontalMovement;
 
     private void OnEnable()
     {
-        playerHorizontalMovement.OnTurn += CheckTurn;
         playerLightResponse.OnChangeResistance += CheckSelfActive;
     }
 
     private void OnDisable()
     {
-        playerHorizontalMovement.OnTurn -= CheckTurn;
         playerLightResponse.OnChangeResistance -= CheckSelfActive;
     }
 
-
-    private void CheckTurn(bool turn) => backSpriteRenderer.flipX = turn;
 
     private void CheckSelfActive(bool activeStatus) => backSpriteRenderer.gameObject.SetActive(activeStatus);
 }
