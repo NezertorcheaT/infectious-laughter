40b429b81fd8dc17e15ac48c74ee2c5c49f51537
SfarxX <alupachiov@mail.ru>
2024-12-04 23:56:21 +0300

    added transportPrefab

--- Assets/Scripts/Inventory/Items/HeartPendant.cs
+++ Assets/Scripts/Inventory/Items/HeartPendant.cs
@@ -6,16 +6,18 @@ using UnityEngine;
 namespace Inventory.Items
 {
     [CreateAssetMenu(fileName = "New Heart Pendant", menuName = "Inventory/Items/Heart Pendant", order = 0)]
-    public class HeartPendant : ScriptableObject, IShopItem, IStartableItem, IEndableItem
+    public class HeartPendant : ScriptableObject, IShopItem, IStartableItem, IEndableItem, ICanSpawn
     {
         public string Name => "Heart Pendant";
         public string Id => "il.heart_pendant";
         public ScriptableObject SelfRef => this;
         public Sprite Sprite => sprite;
         public Sprite SpriteForShop => spriteForShop;
+        public ItemAdderVerifier Verifier { get; set; }
 
         [SerializeField] private Sprite spriteForShop;
         [SerializeField] private Sprite sprite;
+        [SerializeField] private GameObject transportPrefab;
         [field: SerializeField, Min(1)] public int ItemCost { get; private set; } = 2;
         [field: SerializeField, Min(1)] public int MaxStackSize { get; private set; } = 1;
         [field: SerializeField, Min(1)] private int healAmount = 1;
@@ -32,6 +34,8 @@ namespace Inventory.Items
 
         public void OnEnded(Entity.Entity entity, IInventory inventory, ISlot slot)
         {
+            var position = entity.transform.position;
+            var transport = Verifier.Container.InstantiatePrefab(transportPrefab, position, Quaternion.identity, null);
             _currents.Remove(entity, out var c);
             c.Dispose();
         }
