e2fdf652e64ac7c62630482e432c4e705b0f17f0
NezertorcheaT <tigrisha06ps@gmail.com>
2024-12-07 20:31:37 +0300

    k

--- Assets/Scripts/UI/Canvas3DRenderer.cs
+++ Assets/Scripts/UI/Canvas3DRenderer.cs
@@ -13,9 +13,12 @@ namespace UI
 
         private void Start()
         {
+            var r = _camera.MainCamera.ScreenToWorldPoint(new Vector3(0, 0, 0)) -
+                    _camera.MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+            r *= 16f;
             _texture = new RenderTexture(
-                (int)(Screen.width * 144.0 / 1080.0 / 5 * _camera.MainCamera.orthographicSize),
-                (int)(Screen.height * 144.0 / 1080.0 / 5 * _camera.MainCamera.orthographicSize),
+                (int)Mathf.Abs(r.x),
+                (int)Mathf.Abs(r.y),
                 24
             );
             _texture.enableRandomWrite = true;
@@ -23,6 +26,19 @@ namespace UI
             _camera.Camera3D.targetTexture = _texture;
             image.texture = _texture;
             image.color = new Color(1, 1, 1, 1);
+        }
+
+        private static int GCD(int a, int b)
+        {
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+
+            return a | b;
         }
     }
 }
